cc8453f56801142061b1f537ee6d9c5b0489fe4a
Daniel Bertini <danielbertini@gmail.com>
2021-03-22 11:17:46 -0300

    melhorias...

--- services/webhook/stripe.js
+++ services/webhook/stripe.js
@@ -4,12 +4,10 @@ exports.do = (request, response) => {
     var data;
     switch (event.type) {
       case "payment_intent.succeeded":
-        data = event.data.object;
-        console.log("payment_intent.succeeded: ", data);
+        persistIntent(event.data.object);
         break;
       case "payment_intent.created":
-        data = event.data.object;
-        console.log("payment_intent.created: ", data);
+        persistIntent(event.data.object);
         break;
       default:
         console.log(`Unhandled event type ${event.type}`);
@@ -19,4 +17,36 @@ exports.do = (request, response) => {
     console.error(error);
     response.json({ received: false });
   }
+};
+
+const persistIntent = (data) => {
+  return new Promise((resolve, reject) => {
+    try {
+      console.log(data);
+      return resolve();
+      // let document = {
+      //   username: sanitizer.string(params.username),
+      //   _zodiac: ObjectId(zodiac),
+      //   birthday: new Date(
+      //     moment(params.birthday, "DD/MM/YYYY").format("YYYY-MM-DD")
+      //   ),
+      // };
+      // db.collection("intents").updateOne(
+      //   { _id: ObjectId(id) },
+      //   { $set: document },
+      //   { upsert: false },
+      //   (error, result) => {
+      //     if (error) {
+      //       logger.fail(error);
+      //       return reject([request.__("unavailableService"), null]);
+      //     } else {
+      //       return resolve();
+      //     }
+      //   }
+      // );
+    } catch (error) {
+      console.error(error);
+      reject([request.__("unavailableService"), null]);
+    }
+  });
 };
