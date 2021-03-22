fb28c51a875d0aa3cf32f680d43461134925aa4b
Daniel Bertini <danielbertini@gmail.com>
2021-03-22 13:43:30 -0300

    melhorias...

--- services/webhook/stripe.js
+++ services/webhook/stripe.js
@@ -2,11 +2,14 @@ exports.do = (request, response) => {
   try {
     var event = request.body;
     switch (event.type) {
+      case "payment_intent.created":
+        persistIntent(event.data.object);
+        break;
       case "payment_intent.succeeded":
         updateIntent(event.data.object);
         break;
-      case "payment_intent.created":
-        persistIntent(event.data.object);
+      case "charge.succeeded":
+        charge(event.data.object);
         break;
       default:
         console.log(`Unhandled event type ${event.type}`);
@@ -58,6 +61,32 @@ const updateIntent = (data) => {
         }
       }
     );
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+};
+
+const charge = (data) => {
+  try {
+    console.log(data);
+    return;
+    // let document = {
+    //   status: data.status,
+    // };
+    // db.collection("intents").updateOne(
+    //   { intentId: data.id },
+    //   { $set: document },
+    //   { upsert: false },
+    //   (error) => {
+    //     if (error) {
+    //       console.error(error);
+    //       return;
+    //     } else {
+    //       return;
+    //     }
+    //   }
+    // );
   } catch (error) {
     console.error(error);
     return;
