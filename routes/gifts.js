54bd5a3c89cacf47eb5d284edc94d47865561aca
Daniel Bertini <danielbertini@gmail.com>
2021-03-25 18:34:25 -0300

    melhorias...

--- routes/gifts.js
+++ routes/gifts.js
@@ -1,9 +1,13 @@
 module.exports = (() => {
   const routes = express.Router();
 
+  const getSent = require("../services/gifts/getSent");
+  const getReceived = require("../services/gifts/getReceived");
   const getGifts = require("../services/gifts/getGifts");
   const putGift = require("../services/gifts/putGift");
 
+  routes.post("/getSent", cors(corsOptions), getSent.do);
+  routes.post("/getReceived", cors(corsOptions), getReceived.do);
   routes.post("/getGifts", cors(corsOptions), getGifts.do);
   routes.post("/putGift", cors(corsOptions), putGift.do);
 
---
+++ services/gifts/getReceived.js
@@ -0,0 +1,70 @@
+exports.do = (request, response) => {
+  try {
+    jwt.verify(
+      request.headers.authorization,
+      config.jwt.secret,
+      (error, decode) => {
+        if (error) {
+          console.error(error);
+          response.status(403).send({
+            success: false,
+            message: request.__("invalidAccess"),
+          });
+        } else {
+          db.collection("userGifts")
+            .aggregate([
+              {
+                $match: {
+                  _to: ObjectId(decode.id),
+                },
+              },
+              {
+                $lookup: {
+                  from: "users",
+                  localField: "_from",
+                  foreignField: "_id",
+                  as: "user",
+                },
+              },
+              {
+                $lookup: {
+                  from: "gifts",
+                  localField: "_gift",
+                  foreignField: "_id",
+                  as: "gift",
+                },
+              },
+              {
+                $project: {
+                  _id: 1,
+                  created: 1,
+                  "user._id": 1,
+                  "user.username": 1,
+                  "user.picture": 1,
+                  "user.online": 1,
+                  "gift._id": 1,
+                  "gift.name": 1,
+                },
+              },
+            ])
+            .sort({ created: -1 })
+            .skip(request.body.skip ? parseInt(request.body.skip) : 0)
+            .limit(request.body.limit ? parseInt(request.body.limit) : 30)
+            .toArray()
+            .then((result) => {
+              response.status(200).send({
+                success: true,
+                result: result,
+              });
+            });
+        }
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    response.status(500).send({
+      success: false,
+      message: request.__("unavailableService"),
+    });
+  }
+};
