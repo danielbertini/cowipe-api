e4ae84e6e7386a378614f5e1e23a52e77b88de80
Daniel Bertini <danielbertini@gmail.com>
2021-04-09 00:19:45 -0300

    melhorias...

--- utils/logger.js
+++ utils/logger.js
@@ -1,12 +1,11 @@
 var moment = require("moment");
 
-module.exports.log = message => {
-    let time = moment(new Date()).format("DD/MM/YYYY HH:MM:SS")
-    console.log(`@ ${time} - ${JSON.stringify(message)}`)
-}
+module.exports.log = (message) => {
+  let time = moment(new Date()).format("DD/MM/YYYY HH:MM:SS");
+  console.log(`@ ${time} - ${JSON.stringify(message)}`);
+};
 
-module.exports.fail = message => {
-    let time = moment(new Date()).format("DD/MM/YYYY HH:MM:SS")
-    console.log(`@ ${time} - ${message}`)
-    bugsnag.notify(new Error(message))
-}
+module.exports.fail = (message) => {
+  let time = moment(new Date()).format("DD/MM/YYYY HH:MM:SS");
+  console.log(`@ ${time} - ${message}`);
+};
